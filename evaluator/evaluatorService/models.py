a54d9b158d1df88bafb9bffa03be586ebcd19064
Sudhir Gupta <u6458881@anu.edu.au>
2020-04-21 18:18:37 +0530

    Added connection to google cloud datastore

--- evaluator/evaluatorService/models.py
+++ evaluator/evaluatorService/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from google.appengine.ext import db
+
 
 # Create your models here.
+
+class Projects(db.Model):
+    
+    
--- evaluator/evaluatorService/views.py
+++ evaluator/evaluatorService/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from google.cloud import datastore
+import os
 # Create your views here.
+
+def getProjects():
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/sudhir/Downloads/creator-innovation-8998992cc013.json'
+    client = datastore.Client('creator-innovation')
+    query = client.query(kind='ProjectData')
+    id_list = list()
+    for x in list(query.fetch()):
+        id_list.append(x.id)
+    
+    return id_list
+        
