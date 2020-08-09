8111eb0d245020f1431b7c6091392824b8e515d8
Sudhir Gupta <u6458881@anu.edu.au>
2020-08-09 10:06:30 +0530

    changes to outer urls file

--- evaluator/evaluator/urls.py
+++ evaluator/evaluator/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import include,path
+#from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
+    path('',include('evaluatorService.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
